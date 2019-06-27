92e5b15a9511ae2b395ce3e968a5ff5e6111f9ef
tomh333 <s.t.huibers@users.noreply.github.com>
2019-06-27 16:00:21 +0200

    #comment added string labels for area of interest

--- inference.pl
+++ inference.pl
@@ -7,7 +7,7 @@ get_minor_id(MinorName, Id) :- minor(MinorName, Id).
 
 % area of interest
 get_minor_for_area(AOI, Minors) :- 
-    area_of_interest(MID, AOI),
+    is_in_area(MID, AOI),
     get_minor_name(MID, Minors).
 
 get_minors_for_area(Area, Minors) :-
@@ -15,7 +15,10 @@ get_minors_for_area(Area, Minors) :-
     
 areas_of_minor(Minor, AOI) :-
     get_minor_id(Minor, MID),
-    area_of_interest(MID, AOI).
+    is_in_area(MID, AOI).
+
+get_all_area_of_interest(Areas) :-
+    findall(X, area_of_interest(X, _), Areas).
 
 % taal
 get_minor_for_language(Language, Minor) :-
@@ -36,7 +39,7 @@ get_minor_for_period(Period, Minor) :-
 
 % get minor for all criteria
 get_minor(Area, Language, Period, Minor) :-
-    area_of_interest(MID, Area), language(MID, Language), starts(MID, Period), get_minor_name(MID, Minor).
+    is_in_area(MID, Area), language(MID, Language), starts(MID, Period), get_minor_name(MID, Minor).
 
 get_minors(Area, Language, Period, Minors) :-
     findall(X, get_minor(Area, Language, Period, X), Minors).
